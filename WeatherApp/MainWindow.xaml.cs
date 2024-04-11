0d19ff4f1ac1e3690670304608f53983b77823e1
AlVoro25 <yahyaev.tamirlan@yandex.ru>
2024-04-11 17:39:17 +0300

    Добавлен базовый неполный интерфейс

---
+++ WeatherApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WeatherApp;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void ComboBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter) return;
+        
+        if (ComboBox.Items.Count > 0 && ComboBox.Items[0] is ComboBoxItem firstItem)
+                firstItem.Content = ComboBox.Text;
+        else if (ComboBox.Items.Count == 0)
+        {
+            var newItem = new ComboBoxItem
+            {
+                Content = ComboBox.Text
+            };
+            ComboBox.Items.Insert(0, newItem);
+        }
+        
+        ComboBox.IsDropDownOpen = true;
+        e.Handled = true;
+    }
+}
